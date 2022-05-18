53c3c80fbd06fa9bacdade9dee9365623c9b114c
Borislav Todorov <btodorov@axway.com>
2022-05-18 14:18:54 +0300

    initial project structure

---
+++ src/main/java/com/softavail/recordingimporter/RecordingImporterApplication.java
@@ -0,0 +1,13 @@
+package com.softavail.recordingimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordingImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordingImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softavail/recordingimporter/RecordingImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softavail.recordingimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
